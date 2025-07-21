9563a166ad8eac3736658de3c1d0bf5e7ec8fe1a
Axel Closas <axelclosas13@gmail.com>
2025-07-21 04:52:49 -0300

    TP2 - Completo - Se trabajo con validación y sanitización de datos en la ruta post. Se aplicó modularización en el manejo de middlewares

--- routes/superHeroRoutes.mjs
+++ routes/superHeroRoutes.mjs
@@ -1,15 +1,16 @@
-import express from 'express'
-
+import { Router } from 'express'
 import { obtenerSuperheroePorIdController, obtenerTodosLosSuperheroesController, buscarSuperheroesPorAtributoController, obtenerSuperheroesMayoresDe30Controller, agregarSuperheroeController, actualizarSuperheroePorIdController, eliminarSuperheroePorIdController, eliminarSuperheroePorNombreSuperheroeController } from '../controllers/superheroesController.mjs'
+import { agregarValidationRules } from '../middlewares/validationRules.mjs'
+import { handleValidationErros } from '../middlewares/errorMiddleware.mjs'
 
-const router = express.Router()
+const router = Router()
 
 router.get('/heroes', obtenerTodosLosSuperheroesController)
 router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller)
 router.get('/heroes/:id', obtenerSuperheroePorIdController)
 router.get('/heroes/:atributo/:valor', buscarSuperheroesPorAtributoController)
 
-router.post('/heroes', agregarSuperheroeController) // Agregar un superhéroe
+router.post('/heroes', agregarValidationRules(), handleValidationErros, agregarSuperheroeController) // Agregar un superhéroe
 router.put('/heroes/:id', actualizarSuperheroePorIdController) // Actualizar un superhéroe por Id
 
 router.delete('/heroes/:id', eliminarSuperheroePorIdController)
